3d572cb346904ffe830f06864f7d7c485abb88b2
Sindre Froyn <sindre.froyn@gmail.com>
2021-12-05 14:48:18 +0100

    Infinite cycle test

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -59,9 +59,24 @@ void TrafficLight::cycleThroughPhases()
     // > runif(n = 1, min = 4, max = 6)
     // [1] 5.685552
 
-    while(true) {
+    double cycleDuration = 5.68; // duration of a single simulation cycle in ms
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+
+    // init stop watch
+    lastUpdate = std::chrono::system_clock::now();
+    while (true) {
+        // sleep at every iteration to reduce CPU usage
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
+
+        // compute time difference to stop watch
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if (timeSinceLastUpdate >= cycleDuration) {
+            std::cout << "UPDATE CYCLE" << std::endl;
+
+            // reset stop watch for next cycle
+            lastUpdate = std::chrono::system_clock::now();
+        }
+    } // eof simulation loop
 
 }
 
